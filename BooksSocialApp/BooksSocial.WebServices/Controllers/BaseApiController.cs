b05e6eeccce7c61b4f2682b80db700640976fc19
petar-rusev <petar.rusev.87@gmail.com>
2015-04-08 19:22:56 +0300

    Added Abstract Controller Class. It is necessary only to inherit it to create new Controller according to the needs you have. Also in the newly created controller you have to add empty Constructor. For example you can refer to the already created UsersController

---
+++ BooksSocialApp/BooksSocial.WebServices/Controllers/BaseApiController.cs
@@ -0,0 +1,30 @@
+﻿namespace BooksSocial.WebServices.Controllers
+{
+    using System.Web.Http;
+
+    using BooksSocial.Data;
+
+    public abstract class BaseApiController : ApiController
+    {
+        private IBooksSocialData data;
+
+        protected BaseApiController()
+            : this(new BooksSocialData(new BooksSocialDbContext()))
+        {
+
+        }
+
+        protected BaseApiController(IBooksSocialData data)
+        {
+            this.data = data;
+        }
+
+        protected IBooksSocialData Data
+        {
+            get
+            {
+                return this.data;
+            }
+        }
+    }
+}
---
+++ BooksSocialApp/BooksSocial.WebServices/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿namespace BooksSocial.WebServices.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using BooksSocial.Data;
+    using BooksSocial.Models;
+
+    public class UsersController : BaseApiController
+    {
+        public UsersController()
+        {
+
+        }
+
+        public UsersController(IBooksSocialData data)
+            : base(data)
+        {
+
+        }
+
+
+        [HttpGet]
+        [Route("api/users")]
+        public IHttpActionResult GetUsersCount()
+        {
+            var count = this.Data.User.All().Count();
+
+            return this.Ok(count);
+        }
+
+    }
+}
