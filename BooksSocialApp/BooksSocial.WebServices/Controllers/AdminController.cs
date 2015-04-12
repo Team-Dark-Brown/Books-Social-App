36aac71e549a04ab048d3d9671eb3adef8aaa95a
kotow <kotow@abv.bg>
2015-04-12 19:45:45 +0300

    Revert "."
    This reverts commit 6d1e15c89fa4bc4981d7dbfed5b68e9bd36867a9.

--- BooksSocialApp/BooksSocial.WebServices/Controllers/AdminController.cs
+++ BooksSocialApp/BooksSocial.WebServices/Controllers/AdminController.cs
@@ -23,19 +23,11 @@
         [Route("Authors")]
         public IHttpActionResult AddAuthor([FromBody]AdminAuthorBindingModel model)
         {
-            if (!ModelState.IsValid)
-            {
-                return Ok("Inavlid model state.");
-            }
             var author = new Author();
-            author.FirstName = model.FirstName;
-            author.LastName = model.LastName;
-            author.Picture = model.Picture;
-            author.Website = model.Website;
-            author.Information = model.Information;
+            author.Information = model.Information; 
             Data.Author.Add(author);
             Data.SaveChanges();
-            return Ok("The author is added successfully.");
+            return this.StatusCode(System.Net.HttpStatusCode.Accepted);
         }
 
         [Route("Authors/{id}")]
@@ -55,11 +47,7 @@
             {
                 return Ok("Inavlid model state.");
             }
-            var author = Data.Author.Find(id);
-            author.FirstName = model.FirstName;
-            author.LastName = model.LastName;
-            author.Picture = model.Picture;
-            author.Website = model.Website;
+            var author = Data.Author.All().FirstOrDefault(a => a.Id == id);
             author.Information = model.Information;
             Data.Author.Update(author);
             Data.SaveChanges();
@@ -76,128 +64,21 @@
             return this.StatusCode(System.Net.HttpStatusCode.Accepted);
         }
 
-        [Route("Genres/{id}")]
-        public IHttpActionResult deleteGenre(Guid id)
-        {
-            var genre = Data.Genre.Find(id);
-            if (genre == null)
-            {
-                return Ok("Genre not found");
-            }
-            Data.Genre.Delete(genre);
-            Data.SaveChanges();
-            return Ok("Genre deleted successfully.");
-        }
-
-        [HttpPut]
-        [Route("Genres/{id}")]
-        public IHttpActionResult updateGenre(Guid id, [FromBody]AdminGenreBindingModel model)
-        {
-            var genre = Data.Genre.Find(id);
-            if (genre == null)
-            {
-                return Ok("Genre not found");
-            }
-            genre.Name = model.Name;
-            Data.Genre.Update(genre);
-            Data.SaveChanges();
-            return Ok("Genre updated successfully.");
-        }
 
         [Route("Books")]
         public IHttpActionResult AddBook([FromBody]AdminBookBindingModel model)
         {
             var book = new Book();
+            book.Characters = model.Characters;
             book.Title = model.Title;
             foreach (var authorId in model.Authors)
             {
-                var author = Data.Author.Find(authorId);
+                var author = Data.Author.All().Where(a => a.Id == authorId).FirstOrDefault();
                 book.Authors.Add(author);
-            }
-            if (model.Characters != null)
-            {
-                book.Characters = model.Characters;
-            }
-            if (model.CoverImage != null)
-            {
-                book.CoverImage = model.CoverImage;
-            }
-            if (model.Isbn != null)
-            {
-                book.Isbn = model.Isbn;
-            }
-            if (model.Resume != null)
-            {
-                book.Resume = model.Resume;
-            }
-            if (model.Genres != null)
-            {
-                foreach (var genreId in model.Genres)
-                {
-                    var genre = Data.Genre.Find(genreId);
-                    book.Genres.Add(genre);
-                }
             }
             Data.Book.Add(book);
             Data.SaveChanges();
             return Ok("The book is added successfully.");
-        }
-
-        [Route("Books/{id}")]
-        public IHttpActionResult deleteBook(Guid id)
-        {
-            var book = Data.Book.Find(id);
-            if (book == null)
-            {
-                return Ok("Book not found");
-            }
-            Data.Book.Delete(book);
-            Data.SaveChanges();
-            return Ok("Book deleted successfully");
-        }
-
-        [HttpPut]
-        [Route("Books/{id}")]
-        public IHttpActionResult updateBook(Guid id, [FromBody]AdminBookBindingModel model)
-        {
-            var book = Data.Book.Find(id);
-            if (book == null)
-            {
-                return Ok("Book not found");
-            }
-            book.Title = model.Title;
-            foreach (var authorId in model.Authors)
-            {
-                var author = Data.Author.Find(authorId);
-                book.Authors.Add(author);
-            }
-            if (model.Characters != null)
-            {
-                book.Characters = model.Characters;
-            }
-            if (model.CoverImage != null)
-            {
-                book.CoverImage = model.CoverImage;
-            }
-            if (model.Isbn != null)
-            {
-                book.Isbn = model.Isbn;
-            }
-            if (model.Resume != null)
-            {
-                book.Resume = model.Resume;
-            }
-            if (model.Genres != null)
-            {
-                foreach (var genreId in model.Genres)
-                {
-                    var genre = Data.Genre.Find(genreId);
-                    book.Genres.Add(genre);
-                }
-            }
-            Data.Book.Update(book);
-            Data.SaveChanges();
-            return Ok("The book is updated successfully.");
         }
     }
 }
