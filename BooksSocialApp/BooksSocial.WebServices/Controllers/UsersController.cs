20b8427320906ea4f56101dd905f0f443547bef1
kotow <kotow@abv.bg>
2015-04-13 19:10:15 +0300

    ..

--- BooksSocialApp/BooksSocial.WebServices/Controllers/UsersController.cs
+++ BooksSocialApp/BooksSocial.WebServices/Controllers/UsersController.cs
@@ -5,7 +5,9 @@
 
     using BooksSocial.Data;
     using BooksSocial.Models;
+    using System;
 
+    [RoutePrefix("api/User")]
     public class UserController : BaseApiController
     {
         public UserController()
@@ -28,6 +30,53 @@
 
             return this.Ok(count);
         }
+
+        [Route("Books/review")]
+        public IHttpActionResult addBookReview([FromUri]Guid bookId, [FromBody]string review)
+        {
+            var rate = new Review();
+            rate.Book = Data.Book.Find(bookId);
+            rate.User = Data.User.All().FirstOrDefault(u => u.UserName == User.Identity.Name);
+            rate.CreatedOn = new DateTime();
+            //rate.text = review;
+            Data.Review.Add(rate);
+            Data.SaveChanges();
+            return Ok("Rated successfully.");
+        }
+
+        [Route("Books/rate")]
+        public IHttpActionResult addBookRating([FromUri]Guid bookId, [FromBody]int rating)
+        {
+            var rate = new Rating();
+            rate.Book = Data.Book.Find(bookId);
+            rate.User = Data.User.All().FirstOrDefault(u => u.UserName == User.Identity.Name);
+            rate.CreatedOn = new DateTime();
+            //rate.value = rating;
+            Data.Rating.Add(rate);
+            Data.SaveChanges();
+            return Ok("Rated successfully.");
+        }
+
+        //public IHttpActionResult addBookToRead()
+        //{
+        //    //TODO
+        //}
+
+        //public IHttpActionResult markBookAsRead()
+        //{
+        //    //TODO
+        //}
+
+        //public IHttpActionResult markBookAsReading()
+        //{
+        //    //TODO
+        //}
+
+        //public IHttpActionResult getBooks()
+        //{
+        //    //TODO
+        //}
+
 
     }
 }
