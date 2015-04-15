c280903f4a60111057320cb001809e766b90d1ca
AKJ1 <kellerr@abv.bg>
2015-04-16 02:43:42 +0300

    Added a few methods to ctrl

--- BooksSocialApp/BooksSocial.Client/js/controller/homeCtrl.js
+++ BooksSocialApp/BooksSocial.Client/js/controller/homeCtrl.js
@@ -1,34 +1,38 @@
-app.controller('homeCtrl', ['$scope', function($scope){
+app.controller('homeCtrl', ['$scope', 'AuthenticateService', 'DataService', function($scope, AuthenticateService, DataService){
 	$scope.title = "what";
-	$scope.login = function(){
-
+	$scope.login = function(user, pass){
+		AuthenticateService.login(user,pass);
 	};
 
 	$scope.logout = function(){
+		AuthenticateSerivce.logout();
+	};
 
+	$scope.register = function(user,email,pass,confirmPass,phone){
+		AuthenticateService.registerUser(user, email, pass, confimPass, phone);
 	};
 
 	$scope.getBooks = function(){
-
+		DataService.getAllBooks(function(response){
+			$scope.books = response.books;
+		});
 	};
 
-	$scope.filter = function(){
-
+	$scope.getUserShelves = function(userId){
+		DataService.getUserShelves(function(response){
+			$scope.shelves = response.shelves;
+		});
 	};
 
-	$scope.getUserLists = function(){
-
+	$scope.createUserShelf = function(){
+		//TODO
 	};
 
-	$scope.createUserList = function(){
-
-	};
-	
 	$scope.createBook = function(){
-
+		//TODO
 	};
 
 	$scope.createGenre = function(){
-
+		//TODO
 	};
 }]);
