4af4312b7106630c49a54ea05d22fed4a6a81bac
akj1 <kellerr@abv.bg>
2015-04-15 20:29:01 +0300

    Added views

--- BooksSocialApp/BooksSocial.Client/js/app.js
+++ BooksSocialApp/BooksSocial.Client/js/app.js
@@ -7,7 +7,11 @@ app.config(['$routeProvider', '$locationProvider', '$filterProvider', function($
 		when('/', { templateUrl: 'views/public/home.html', controller: 'homeCtrl'}).
 		when('/login', { templateUrl: 'views/public/login.html', controller: 'homeCtrl'}).
 		when('/register', {	templateUrl: 'views/public/register.html',	controller: 'homeCtrl'}).
-		when('/user/books', {templateUrl: 'views/user/books.html', controller: 'homeCtrl'}).
-		when('/logout', {redirectTo: 'views/user/logout.html'}).
+		when('/user/books', { templateUrl: 'views/user/books.html', controller: 'homeCtrl'}).
+		when('/admin/add-book', { templateUrl : 'views/admin/book-editor.html', controller: 'homeCtrl'}).
+		when('/admin/add-genre', { templateUrl : 'views/admin/genre-editor.html', controller: 'homeCtrl'}).
+		when('/user/add-list', { templateUrl : 'views/user/reading-list-editor.html', controller: 'homeCtrl'}).
+		when('/user/view-lists', { templateUrl : 'views/user/list-of-reading-lists.html', controller: 'homeCtrl'}).
+		when('/logout', { templateUrl: 'views/public/logout.html', controller: 'homeCtrl'}).
 		otherwise({ redirectTo: "/"	});
 }]);
