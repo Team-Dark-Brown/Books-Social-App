491962483b5003c544f248ca16f1f8c2a0663246
JulietaAtanasova <julieta.p.atanasova@gmail.com>
2015-04-15 23:06:01 +0300

    Add get all authors.

---
+++ BooksSocialApp/BooksSocial.WebServices/Controllers/AuthorController.cs
@@ -0,0 +1,36 @@
+﻿namespace BooksSocial.WebServices.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using BooksSocial.Data;
+
+    public class AuthorController : BaseApiController
+    {
+        public AuthorController()
+        {
+        }
+
+        public AuthorController(IBooksSocialData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        [Route("api/authors")]
+        public IHttpActionResult GetAllAuthors()
+        {
+            var authors = Data.Author.All().Select(a => new
+            {
+                Id = a.Id,
+                FirstName = a.FirstName,
+                LastName = a.LastName,
+                Information = a.Information,
+                Picture = a.Picture,
+                Website = a.Website
+            });
+
+            return Ok(authors);
+        }
+    }
+}
