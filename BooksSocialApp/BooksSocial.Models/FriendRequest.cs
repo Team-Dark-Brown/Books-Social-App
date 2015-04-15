1fe0cea18678d4098e921e8194cc2cc17b3ec4fa
JulietaAtanasova <julieta.p.atanasova@gmail.com>
2015-04-16 02:40:25 +0300

    Add friend request model.

---
+++ BooksSocialApp/BooksSocial.Models/FriendRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSocial.Models
+{
+    public class FriendRequest
+    {
+        public FriendRequest()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public virtual User Sender { get; set; }
+
+        public virtual User Receiver { get; set; }
+    }
+}
--- BooksSocialApp/BooksSocial.WebServices/Controllers/BaseApiController.cs
+++ BooksSocialApp/BooksSocial.WebServices/Controllers/BaseApiController.cs
@@ -1,17 +1,25 @@
 ﻿namespace BooksSocial.WebServices.Controllers
 {
+    using System.Web;
     using System.Web.Http;
 
-    using BooksSocial.Data;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using BooksSocial.Data;    
+    using BooksSocial.Models;
 
     public abstract class BaseApiController : ApiController
     {
         private IBooksSocialData data;
 
+        protected BooksSocialDbContext context;
+
         protected BaseApiController()
             : this(new BooksSocialData(new BooksSocialDbContext()))
         {
-
+            var userManager = new UserManager<User>(new UserStore<User>((this.Data as BooksSocialData).context));
+            this.CurrentUser = userManager.FindById(HttpContext.Current.User.Identity.GetUserId());
         }
 
         protected BaseApiController(IBooksSocialData data)
@@ -19,12 +27,11 @@
             this.data = data;
         }
 
+        protected User CurrentUser { get; private set; }
+
         protected IBooksSocialData Data
         {
-            get
-            {
-                return this.data;
-            }
+            get { return this.data; }
         }
     }
 }
